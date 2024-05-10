f14744de10ec261f5a2098621a667785ae97a632
Joao-J <joaovitor19.gbi@gmail.com>
2024-05-10 14:03:33 -0300

    Agora vai

--- scritp.js
+++ scritp.js
@@ -2,5 +2,36 @@
 const PromptSync = require('prompt-sync')({sigint: true});
 
 var entrada = PromptSync("alguem ai?");
-console.log(entrada);
+var login = [];
 
+var loginNome;
+var LoginSenha;
+
+
+function Msg(msg){
+  console.log(msg);  
+};
+
+function start(){
+    if(login.length > 0){
+        Msg("===================\n 1 - LOGIN \n 2 - REGISTER \n\n===================\n Escolha entre as opções: ");
+        var escolha = PromptSync();
+        switch (esolha){
+          case '1\n':
+          Msg("1");
+          break;
+          case '2\n':
+          Msg("2");
+          break;
+        };
+    }else{
+        Msg("Crie um novo personagem: ");
+        var nome = PromptSync('Nome:');
+        var senha = PromptSync('Senha');
+        login += {nome,senha};
+    };
+};
+
+
+console.log(entrada);
+start();
