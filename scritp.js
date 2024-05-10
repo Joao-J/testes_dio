55864a435fa7951df1ffcc89705e66a37019c0eb
Joao-J <joaovitor19.gbi@gmail.com>
2024-05-10 15:11:33 -0300

    agora sim atualizado e com um banco de dados improvisado

--- scritp.js
+++ scritp.js
@@ -1,8 +1,11 @@
 
 const PromptSync = require('prompt-sync')({sigint: true});
 
+var starting = true;
+
 var entrada = PromptSync("alguem ai?");
-var login = [];
+var db = [];
+
 
 var loginNome;
 var LoginSenha;
@@ -12,25 +15,55 @@ function Msg(msg){
   console.log(msg);  
 };
 
+function login(name,pass){
+    console.clear
+    for(usercount = 0; db.length > usercount; usercount++){
+        var user = db[usercount]
+        nameDb = user[0];
+        passDb = user[1];
+        score = user[2];
+        Msg(nameDb + " " + passDb + "\n")
+        if (name == nameDb && pass == passDb){
+            Msg("Logado com Sucesso");
+            break;
+        }
+        if ((usercount + 1) == db.length){
+           Msg("Usuario ou Senha não encontrado"); 
+        }
+    }
+}
+
+function createUser(){
+    console.clear
+    Msg("Crie um novo personagem: ");
+    var nome = PromptSync('Nome:');
+    var senha = PromptSync('Senha: ');
+    db.push([nome,senha,0]);
+    console.clear
+    Msg("Usuario Criado com Sucesso!!");
+}
+
 function start(){
-    if(login.length > 0){
-        Msg("===================\n 1 - LOGIN \n 2 - REGISTER \n\n===================\n Escolha entre as opções: ");
-        var escolha = PromptSync();
-        switch (esolha){
-          case '1\n':
-          Msg("1");
-          break;
-          case '2\n':
-          Msg("2");
-          break;
+    while(starting == true){
+        if(db.length > 0){
+            Msg("===================\n 1 - LOGIN \n 2 - REGISTER \n\n 0 - EXIT \n===================\n Escolha entre as opções: ");
+            var escolha = PromptSync();
+            switch (escolha){
+            case '1':
+            login(PromptSync('Nome: ') , PromptSync('Senha: '));
+            break;
+            case '2':
+            createUser();
+            break;
+            case '0':
+                starting = false;
+            };
+        }else{
+            createUser();
         };
-    }else{
-        Msg("Crie um novo personagem: ");
-        var nome = PromptSync('Nome:');
-        var senha = PromptSync('Senha');
-        login += {nome,senha};
     };
 };
+
 
 
 console.log(entrada);
