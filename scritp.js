5e595630d02474b6c3a87970e1e1379a583772fa
Joao-J <joaovitor19.gbi@gmail.com>
2024-05-13 15:27:33 -0300

    anterior

--- scritp.js
+++ scritp.js
@@ -1,6 +1,10 @@
-nomeHeroi = ""
-pontosHeroi = ""
-senhaHeroi = ""
+const { stdout } = require('process');
+
+var  nomeHeroi = "";
+var pontosHeroi = "";
+var senhaHeroi = "";
+
+var bancoDados = [];
 
 function jogar(){
     
@@ -10,7 +14,21 @@ function puxarPontos(){
 
 }
 
+const Console = require('readline').createInterface({
+entrada:process.stdin,
+saida:process.stdout
+});
+
 function login(){
-    logado = false 
+    logado = false;
+    console.log("LOGIN \n");
+    let usuario = "";
+    Console.question("USUARIO: ", usuario);
+    console.log("foi feito " + usuario);
+    for(dados in bancoDados){
+
+    } 
     return logado;
 }
+
+login();
